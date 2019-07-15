21757f85f73ebe9b505d295cd3ec6ea5d9fdcfd9
Mohm4D <mohmadmalshami@yahoo.com>
2019-07-15 19:24:56 +0430

    some change

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,19 +1,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
 
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}" >
+    <link rel="stylesheet" href="{{asset('assets/css/custom-bootstrap-rtl.css')}}">
+    <!-- chartist CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/chartist.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/chartist-init.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/chartist-plugin-tooltip.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/css-chart.css')}}">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- You can change the theme colors from here -->
+    <link rel="stylesheet" href="{{asset('assets/css/blue.css')}}s" id="theme">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+   {{-- <script src="{{ asset('js/app.js') }}" defer></script>--}}
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link rel="stylesheet" type="text/css" href="{{url('https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')}}">
+
 </head>
+
--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,28 +1,28 @@
-<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<script src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.js')}}"></script>
 <!-- Bootstrap tether Core JavaScript -->
-<script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
-<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{asset('assets/js/tether.min.js')}}"></script>
 <!-- slimscrollbar scrollbar JavaScript -->
-<script src="js/jquery.slimscroll.js"></script>
+<script src="{{asset('assets/js/jquery.slimscroll.js')}}"></script>
 <!--Wave Effects -->
-<script src="js/waves.js"></script>
+<script src="{{asset('assets/js/waves.js')}}"></script>
 <!--Menu sidebar -->
-<script src="js/sidebarmenu.js"></script>
+<script src="{{asset('assets/js/sidebarmenu.js')}}"></script>
 <!--stickey kit -->
-<script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<script src="{{asset('assets/js/stickey-kit.min.js')}}"></script>
 <!--Custom JavaScript -->
-<script src="js/custom.min.js"></script>
+<script src="{{asset('assets/js/custom.min.js')}}"></script>
 <!-- ============================================================== -->
 <!-- This page plugins -->
 <!-- ============================================================== -->
 <!-- chartist chart -->
-<script src="../assets/plugins/chartist-js/dist/chartist.min.js"></script>
-<script src="../assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+<script src="{{asset('assets/js/chartist.min.js')}}"></script>
+<script src="{{asset('assets/js/chartist-plugin-tooltip.min.js')}}"></script>
 <!-- Chart JS -->
-<script src="../assets/plugins/echarts/echarts-all.js"></script>
+<script src="{{asset('assets/js/echarts-all.js')}}"></script>
 <!-- Chart JS -->
-<script src="js/dashboard1.js"></script>
+<script src="{{asset('assets/js/dashboard1.js')}}"></script>
 <!-- ============================================================== -->
 <!-- Style switcher -->
 <!-- ============================================================== -->
-<script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+<script src="{{asset('assets/js/jQuery.style.switcher.js')}}"></script>
