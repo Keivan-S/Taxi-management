563fc74cdcceafd665314e4a4803f8731bb560e3
Mohm4D <mohammad5248@yahoo.com>
2019-07-23 18:15:02 +0430

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,6 @@
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'api/v1', 'middleware' => 'auth' ], function (){
-    Route::get('users/{user}', function (App\User $user){
-        return $user;
-    });
-});
-
-Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function (){
-    Route::get('/', 'HomeController@dashboard')->name('dashboard');
-});
 
 Route::get('/dashboard', 'HomeController@dashboard')->name('home');
 
@@ -20,13 +11,9 @@ Route::prefix('validate')->group(function () {
     Route::post('mobile_no', 'Auth\LoginController@validateMobile');
 });
 
-
-Route::get('/', 'HomeController@index')->name('home');
-
 Route::prefix('roles')->group(function () {
     Route::get('/', 'HomeController@roles')->name('roles');
 });
-
 
 Route::prefix('ajax')->group(function () {
     Route::prefix('roles')->group(function () {
@@ -35,8 +22,9 @@ Route::prefix('ajax')->group(function () {
     Route::get('/', 'HomeController@roles')->name('roles');
 });
 
-Route::group(['prefix' => 'api/v1', 'middleware' => 'auth' ], function (){
 
+// Api Routs
+Route::group(['prefix' => 'api/v1', 'middleware' => 'auth' ], function (){
     Route::get('users/{user}', function (App\User $user){
         return $user;
     });
@@ -44,45 +32,3 @@ Route::group(['prefix' => 'api/v1', 'middleware' => 'auth' ], function (){
 
 
 
-Route::get('/dashboard', 'HomeController@dashboard')->name('home');
-
-Route::prefix('validate')->group(function () {
-    Route::post('mobile_no', 'Auth\LoginController@validateMobile');
-});
-
-
-Route::get('/', 'HomeController@index')->name('home');
-
-Route::prefix('roles')->group(function () {
-    Route::get('/', 'HomeController@roles')->name('roles');
-});
-
-
-Route::prefix('ajax')->group(function () {
-    Route::prefix('roles')->group(function () {
-        Route::get('/data', 'RoleController@ajaxdata');
-    });
-    Route::get('/', 'HomeController@roles')->name('roles');
-});
-
-
-
-Auth::routes();
-
-Route::prefix('validate')->group(function () {
-    Route::post('mobile_no', 'Auth\LoginController@validateMobile');
-});
-
-
-
-Route::prefix('roles')->group(function () {
-    Route::get('/', 'HomeController@roles')->name('roles');
-});
-
-
-Route::prefix('ajax')->group(function () {
-    Route::prefix('roles')->group(function () {
-        Route::get('/data', 'RoleController@ajaxdata');
-    });
-    Route::get('/', 'HomeController@roles')->name('roles');
-});
